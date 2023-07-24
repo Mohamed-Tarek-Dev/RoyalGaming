029fa009e419ccab5aca7a8ed30d4b64b66bb791
Mohamed Tarek <63224925+Mohamed2815@users.noreply.github.com>
2023-07-25 00:29:00 +0300

    last

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: "en",
-      fallbackLocale: "en",
-      localeDir: "locales",
-      enableLegacy: false,
-      runtimeOnly: false,
-      compositionOnly: false,
-      fullInstall: true,
-    },
-  },
-};
+    publicPath: '/vue-pages/'
+}
